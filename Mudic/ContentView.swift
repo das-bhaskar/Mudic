ecb3a309439bc7f96b6ab8aefa4e1793898f23ec
aloofjelly <aloofjelly@gmail.com>
2025-04-11 14:41:06 -0400

    Initial Commit

---
+++ Mudic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mudic
+//
+//  Created by Bhaskar Das on 2025-04-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mudic/MudicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MudicApp.swift
+//  Mudic
+//
+//  Created by Bhaskar Das on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct MudicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
